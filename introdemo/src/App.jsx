134dd703fb7164cfe6ba0660bbc0456980574c57
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-21 16:09:10 -0600

    Add introdemo progress from part2c

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,28 +1,58 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Note from './components/Note';
 
-function Hello(props) {
-  console.log(props);
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('promise fullfilled');
+      setNotes(response.data);
+    });
+  }, []);
+  console.log('render', notes.length, 'notes');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important === true);
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   );
-}
+};
 
-function App() {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Arpan' age="34" />
-      <Hello name='Pat' age="33"/>
-      <Hello name='Jordan' age={2025 - 1845}/>
-    </div>
-  );
-}
-
-export default App
+export default App;
