91590f0b9cd493c07b7a8827279f9a5917de57ed
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-26 21:41:49 -0600

    Update backend for part3d

--- backend/index.js
+++ backend/index.js
@@ -52,14 +52,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    });
-  }
 
   const note = new Note({
     content: body.content,
@@ -68,18 +62,18 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(result => {
     response.json(result);
-  });
+  })
+  .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       if (response) {
         response.json(updatedNote);
@@ -101,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformed id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
