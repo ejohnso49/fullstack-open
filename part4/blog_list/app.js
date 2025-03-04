dc15bcded42707cdf42ddfd5f86f5794843b6988
Eric Johnson <eric.j.johnson22@gmail.com>
2025-03-03 23:18:32 -0600

    Complete exercise 4.2

---
+++ part4/blog_list/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+mongoose.set("strictQuery", false);
+
+logger.info("Connecting to", config.MONGODB_CONNECTION_STRING);
+
+mongoose.connect(config.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+module.exports = app;
