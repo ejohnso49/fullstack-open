c650f3b3fcec1244efff58cd3b6e6a41b1325acd
Eric Johnson <eric.j.johnson22@gmail.com>
2025-03-04 21:55:47 -0600

    Complete exercise 4.5

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -55,3 +55,74 @@ describe("totalLikes", () => {
     assert.strictEqual(result, 0);
   });
 });
+
+describe("favoriteBlog", () => {
+  const blogListSingleHighest = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b523676234d17f8",
+      title: "Go Harm",
+      author: "Ed W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a4223a71b523676234d17f8",
+      title: "Go To d Harmful",
+      author: "Edsger W. Ijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  const blogListMultipleHighest = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b523676234d17f8",
+      title: "Go Harm",
+      author: "Ed W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+    {
+      _id: "5a4223a71b523676234d17f8",
+      title: "Go To d Harmful",
+      author: "Edsger W. Ijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 0,
+      __v: 0,
+    },
+  ];
+
+  test("empty lists returns favorite as undefined", () => {
+    const result = listHelper.favoriteBlog([]);
+    assert.deepStrictEqual(result, undefined);
+  });
+
+  test("returns the highest liked items", () => {
+    const result = listHelper.favoriteBlog(blogListSingleHighest);
+    assert.deepStrictEqual(result, blogListSingleHighest[1]);
+  });
+
+  test("returns the first item if multiple have same number of likes", () => {
+    const result = listHelper.favoriteBlog(blogListMultipleHighest);
+    assert.deepStrictEqual(result, blogListMultipleHighest[0]);
+  });
+});
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,7 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length < 1) {
+    return undefined;
+  }
+
+  return blogs.reduce((currentMaxBlog, blog) => {
+    if (blog.likes > currentMaxBlog.likes) {
+      return blog;
+    } else {
+      return currentMaxBlog;
+    }
+  }, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
