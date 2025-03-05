be33598907bf96a2c271f4a5360110ea4dc028ca
Eric Johnson <eric.j.johnson22@gmail.com>
2025-03-04 22:41:29 -0600

    Complete exercise 4.6

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -113,7 +113,7 @@ describe("favoriteBlog", () => {
 
   test("empty lists returns favorite as undefined", () => {
     const result = listHelper.favoriteBlog([]);
-    assert.deepStrictEqual(result, undefined);
+    assert.strictEqual(result, undefined);
   });
 
   test("returns the highest liked items", () => {
@@ -124,5 +124,84 @@ describe("favoriteBlog", () => {
   test("returns the first item if multiple have same number of likes", () => {
     const result = listHelper.favoriteBlog(blogListMultipleHighest);
     assert.deepStrictEqual(result, blogListMultipleHighest[0]);
+  });
+});
+
+describe("mostBlogs", () => {
+  const blogsSingleMultiPostAuthor = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b523676234d17f8",
+      title: "Go Harm",
+      author: "Ed W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a4223a71b523676234d17f8",
+      title: "Go To d Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  const blogsMultiPostAuthors = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b523676234d17f8",
+      title: "Go Harm",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+    {
+      _id: "5a4223a71b523676234d17f8",
+      title: "Go To d Harmful",
+      author: "Edsger W. Ijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a42353a71b523676234d17f8",
+      title: "Harm",
+      author: "Edsger W. Ijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 0,
+      __v: 0,
+    },
+  ];
+
+  test("returns undefined for empty blog list", () => {
+    const result = listHelper.mostBlogs([]);
+    assert.strictEqual(result, undefined);
+  });
+
+  test("returns single most popular author", () => {
+    const result = listHelper.mostBlogs(blogsSingleMultiPostAuthor);
+    assert.deepStrictEqual(result, { author: blogsSingleMultiPostAuthor[0].author, blogs: 2 });
+  });
+
+  test("returns first author with the most number of posts", () => {
+    const result = listHelper.mostBlogs(blogsMultiPostAuthors);
+    assert.deepStrictEqual(result, { author: blogsMultiPostAuthors[0].author, blogs: 2 });
   });
 });
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -20,8 +20,31 @@ const favoriteBlog = (blogs) => {
   }, blogs[0]);
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length < 1) {
+    return undefined;
+  }
+
+  const blogCountMap = blogs.reduce((blogCount, blog) => {
+    const currentCount = blogCount[blog.author] || 0;
+    blogCount[blog.author] = currentCount + 1;
+    console.log(`${blog.author}: ${blogCount[blog.author]}`);
+    return blogCount;
+  }, {});
+
+  return Object.keys(blogCountMap).reduce((result, author) => {
+    if (blogCountMap[author] > result.blogs) {
+      result.author = author;
+      result.blogs = blogCountMap[author];
+    }
+
+    return result;
+  }, { author: "", blogs: 0 });
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
