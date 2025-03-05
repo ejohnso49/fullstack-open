126bf3020d024e52856ce44dd1b8b075acbd5420
Eric Johnson <eric.j.johnson22@gmail.com>
2025-03-04 21:30:55 -0600

    Complete exercise 4.4

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -10,3 +10,48 @@ describe("dummy", () => {
     assert.strictEqual(result, 1);
   });
 });
+
+describe("totalLikes", () => {
+  const blogList = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b523676234d17f8",
+      title: "Go Harm",
+      author: "Ed W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 4,
+      __v: 0,
+    },
+    {
+      _id: "5a4223a71b523676234d17f8",
+      title: "Go To d Harmful",
+      author: "Edsger W. Ijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  test("when list has one blog, likes equal to single blogs", () => {
+    const listWithOneBlog = blogList.slice(0, 1);
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, listWithOneBlog[0].likes);
+  });
+
+  test("of a list with multiple items", () => {
+    const result = listHelper.totalLikes(blogList);
+    assert.strictEqual(result, 5 + 4 + 3);
+  });
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+});
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
