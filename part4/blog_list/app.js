79d06ce9cbbdaa22ff644842178704f72cfb19d5
Eric Johnson <eric.j.johnson22@gmail.com>
2025-04-12 23:48:32 -0500

    Complete exercises 4.15 + 4.16

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -6,6 +6,7 @@ require("express-async-errors");
 
 const app = express();
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const { errorHandler } = require("./utils/middleware");
@@ -23,6 +24,7 @@ mongoose.connect(config.MONGODB_CONNECTION_STRING)
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
 module.exports = app;
---
+++ part4/blog_list/tests/users_api.test.js
@@ -0,0 +1,108 @@
+const bcrypt = require("bcrypt");
+const { describe, test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const User = require("../models/user");
+const helper = require("./test_helper");
+
+const api = supertest(app);
+
+describe("addition of a user", () => {
+  beforeEach(async () => {
+    const sleepPromise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 2000);
+    });
+    await sleepPromise;
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
+  });
+
+  test("creation succeeds with new username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "ejohnso49",
+      name: "Eric",
+      password: "silly",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(user => user.username);
+    assert(usernames.includes(newUser.username));
+  });
+
+  test("creation fails with proper status code and message if username already exists", async () => {
+    const usersAtStart = await helper.usersInDb();
+    assert.strictEqual(usersAtStart[0].username, "root");
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "rootsy",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(result.body.error.includes("expected \"username\" to be unique"));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test("creation fails if username or password are too short", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    let newUser = {
+      username: "no",
+      name: "I'm gonna fail",
+      password: "doesn't matter",
+    };
+
+    let result = await api.post("/api/users").send(newUser).expect(400).expect("Content-Type", /application\/json/);
+    assert(result.body.error.includes("Username too short"));
+
+    newUser = {
+      username: "better",
+      name: "Gonna fail too",
+      password: "no",
+    };
+
+    result = await api.post("/api/users").send(newUser).expect(400).expect("Content-Type", /application\/json/);
+    assert(result.body.error.includes("Password too short"));
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test("get users retrieves expected list", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const result = await api.get("/api/users");
+    assert.deepStrictEqual(result.body, usersAtStart);
+  });
+});
+
+after(async () => {
+  await User.deleteMany({});
+  await mongoose.connection.close();
+});
